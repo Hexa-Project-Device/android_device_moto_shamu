333047675bc764aec8f14fb3d92c3be2d049e9bf
camcory <camcory1@gmail.com>
2018-08-07 20:16:57 -0400

    Lets get this building

--- device.mk
+++ device.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
     libxml2
 
 # NFC packages
+<<<<<<< HEAD
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     nfc_nci.bcm2079x.default \
@@ -196,6 +197,19 @@ PRODUCT_COPY_FILES += \
 # Sec config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+=======
+#PRODUCT_PACKAGES += \
+#    nfc_nci.bcm2079x.default \
+#    NfcNci \
+#    Tag
+
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+#    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+#    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
+#    device/moto/shamu/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+#    device/moto/shamu/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
+>>>>>>> 9025d805... Lets get this building
 
 # Thermal config
 PRODUCT_COPY_FILES += \
